36d57e102263a14e613741a7afc16a9ffefe1e4f
DaisyDogs07 <bridgeselijah07@gmail.com>
2023-10-28 22:59:29 -0400

    Update FileSystem.h

--- FileSystem.h
+++ FileSystem.h
@@ -2132,14 +2132,13 @@ class FileSystem {
       range->size = newRangeLength;
       if (offset + length > size)
         size = offset + length;
-      for (off_t i = rangeIdx + 1; i < dataRangeCount;) {
-        struct DataRange* range2 = dataRanges[i];
-        if (offset + length < range2->offset)
-          break;
-        if (offset + length < range2->offset + range2->size) {
+      while (rangeIdx + 1 < dataRangeCount) {
+        struct DataRange* range2 = dataRanges[rangeIdx + 1];
+        if (offset + length >= range2->offset &&
+            offset + length < range2->offset + range2->size) {
           memcpy(range->data + (range2->offset - range->offset), range2->data, range2->size);
-          RemoveRange(i);
-        } else ++i;
+          RemoveRange(rangeIdx + 1);
+        } else break;
       }
       return range;
     }
